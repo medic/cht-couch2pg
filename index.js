513d0c3d1161ef2da9de91de31e04de4fe09b317
Victor Gomez <victor@medicmobile.org>
2018-04-11 14:24:37 -0600

    Updating to support medic and medic-sentinel replication

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ var Promise = require('rsvp').Promise,
     env = require('./env')(),
     xmlformsMigrator = require('./libs/xmlforms/migrator');
 
+var sentinelUrl = env.couchdbUrl + '-sentinel';
+
 var couchdb = require('pouchdb')(env.couchdbUrl),
+    sentinel = require('pouchdb')(sentinelUrl),
     db = require('pg-promise')({ 'promiseLib': Promise })(env.postgresqlUrl);
+
+// Removes credentials from couchdb url
+// Converts http://admin:pass@localhost:5984/couch1
+// to localhost:5984/couch1 -- seq source
+var parseSource = function(url) {
+  var source = urlParser.parse(url);
+  return source.host+source.path;
+};
 
 var couch2pg = require('couch2pg'),
     migrator = couch2pg.migrator,
     importer = couch2pg.importer(
       db, couchdb,
       env.couch2pgDocLimit,
-      env.couch2pgChangesLimit),
+      env.couch2pgChangesLimit,
+      parseSource(env.couchdbUr)),
+    sentinelImporter = couch2pg.importer(
+      db, sentinel,
+      env.couch2pgDocLimit,
+      env.couch2pgChangesLimit,
+      parseSource(sentinelUrl)),
     xmlforms = require('./libs/xmlforms/updater')(db);
 
 var firstRun = false;
@@ -58,7 +75,7 @@ var run = function() {
   log.info('Beginning couch2pg and xmlforms run at ' + new Date());
 
   var runErrored = false;
-  return importer.importAll()
+  return Promise.all[importer.importAll(), sentinel.importAll()]
   .catch(function(err) {
     log.error('Couch2PG import failed');
     log.error(err);
@@ -68,12 +85,16 @@ var run = function() {
     }
     runErrored = true;
   })
-  .then(function(results) {
+  .then(function(allResults) {
+    var results = allResults[0];
+    var sentinelResults = allResults[1];
     // Run secondary tasks if we reasonably think there might be new data
     // runErrored || errorCount <- something went wrong, but maybe there is still new data
     // firstRun <- this is first run, we don't know what the DB state is in
     // results.deleted.length || results.edited.length <- data has changed
-    if (runErrored || errorCount || firstRun || results.deleted.length || results.edited.length) {
+    if (runErrored || errorCount || firstRun ||
+        results.deleted.length || results.edited.length ||
+        sentinelResults.deleted.length || sentinelResults.edited.length) {
       return xmlforms.update();
     }
   })
