ddd581a0a5f77f17303aac0c1a0a85e1e43e91be
Victor Gomez <victor@medicmobile.org>
2018-05-03 09:43:24 -0700

    Adding support for advanced watch mode against local dbs or docker-compose and setting travis with node 8/9

---
+++ jest-test.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  displayName: 'jest:test',
+  'testEnvironment': 'node',
+  'testPathIgnorePatterns': [
+    'bootstrap', 'mocks', 'docs', 'utils'
+  ],
+  'testMatch': ['<rootDir>/spec/**/*.js'],
+  'collectCoverageFrom': ['<rootDir>/replicate/**/*.{js}'],
+  'coverageThreshold': {
+    'global': {
+      'branches': 100,
+      'functions': 100,
+      'lines': 100,
+      'statements': 100
+    }
+  }
+}
