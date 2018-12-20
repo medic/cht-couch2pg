1203b206c39f3e462cf0915a8c5d928235bcabd9
Victor Gomez <victor@medicmobile.org>
2018-12-20 13:16:49 -0700

    Cleaning safe function

--- libs/medic/safe.js
+++ libs/medic/safe.js
@@ -1,11 +1,17 @@
+const EXPECTED_URLS = ['couchdbUrl', 'postgresqlUrl'];
+
+const cleanUrl = (url) => {
+  if(url && url.split('@').length>0) {
+    return url.split('@').pop();
+  }
+  return url;
+}
+
 const safe = (opts) => {
   const o = Object.assign({}, opts);
-  if(o.couchdbUrl && o.couchdbUrl.split('@').length>0) {
-    o.couchdbUrl = o.couchdbUrl.split('@').pop();
-  }
-  if(o.postgresqlUrl && o.postgresqlUrl.split('@').length>0) {
-    o.postgresqlUrl = o.postgresqlUrl.split('@').pop();
-  }
+  EXPECTED_URLS.forEach(url => {
+    o[url] = cleanUrl(o[url]);
+  })
   return o;
 };
 
