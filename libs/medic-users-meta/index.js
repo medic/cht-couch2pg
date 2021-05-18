a0d70ee28e06e317b8c00cd8e9b6ca936a6b91ba
Mariano Ruiz <mrsarm@gmail.com>
2021-05-18 19:11:18 -0300

    Revert last changes

--- libs/medic-users-meta/index.js
+++ libs/medic-users-meta/index.js
@@ -1,45 +1,15 @@
 const Postgrator = require('postgrator');
 const log = require('loglevel');
 
-const migrate = async (postgresUrl) => {
-
-  const postgratorConfig = {
+const migrate = postgresUrl => {
+  const postgrator = new Postgrator({
+    migrationDirectory: `${__dirname}/migrations`,
     schemaTable: 'users_meta_migrations',
     driver: 'pg',
     logProgress: log.getLevel() <= log.levels.DEBUG,
     connectionString: postgresUrl,
-  };
-
-  const postgrator = new Postgrator({
-    ...postgratorConfig,
-    migrationDirectory: `${__dirname}/migrations`,
   });
 
-  let lastMigrationExecuted = '';
-  try {
-    lastMigrationExecuted = await postgrator.getDatabaseVersion();
-  } catch (e) {
-    if (e.message !== 'relation "users_meta_migrations" does not exist') {
-      throw e;  // Unknown error
-    }
-    // First time migrations for medic-users-meta are executed, so
-    // migrations under ./migrations/deprecated were not executed,
-    // therefore no need to execute ./migrations/patch scripts
-  }
-  if (lastMigrationExecuted === '202102191153') {
-    // The first migration that contains bugs was executed earlier,
-    // the migrations under ./migrations/patch are executed to drop
-    // the views without dropping the tables with the data
-    const postgratorPatch = new Postgrator({
-      ...postgratorConfig,
-      migrationDirectory: `${__dirname}/migrations/patch`,
-    });
-    await postgratorPatch.migrate();
-  }
-  // Regardless of ./migrations/patch/* migrations being executed
-  // or not, the migrations at ./migrations are executed with
-  // the creation of the table to hold the meta data (if doesn't
-  // exist already) and the views with the right definitions
   return postgrator.migrate();
 };
 
--- libs/medic-users-meta/migrations/patch/202105171932.do.86.dropUsersMetaViews.sql
+++
@@ -1,7 +0,0 @@
--- Drop views with wrong indexes and definitions that were
--- created at ../deprecated/202102191153.do.54-create-couchdb-users-meta-table.sql
---
--- The views are created again at ../202105171933.do.86.userMetaViews.sql
-
-DROP MATERIALIZED VIEW IF EXISTS useview_feedback;
-DROP MATERIALIZED VIEW IF EXISTS useview_telemetry;
