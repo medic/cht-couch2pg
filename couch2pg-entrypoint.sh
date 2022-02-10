7190be3e9f8fa54d13abd5f37ca3bf74cf5f80e7
mrjones-plip <mrjones-plip@plip.com>
2022-02-10 15:37:56 -0800

    Debug branch - ensure entry point works happy path external couchdb

--- couch2pg-entrypoint.sh
+++ couch2pg-entrypoint.sh
@@ -16,6 +16,8 @@ set_postgres_url(){
 check_if_postgres_is_ready(){
   #waiting for postgres
   wait_count=0
+  echo "export POSTGRESQL_URL=postgres://$POSTGRES_USER_NAME:$POSTGRES_PASSWORD@$POSTGRES_DB:5432/$POSTGRES_DB_NAME"
+  echo "pg_isready -q  -h $POSTGRES_DB -U $POSTGRES_USER_NAME --d $POSTGRES_DB_NAME"
   until  pg_isready -q  -h $POSTGRES_DB -U $POSTGRES_USER_NAME --d $POSTGRES_DB_NAME
   do
     echo "Waiting for PostgreSQL..." >&2
@@ -33,7 +35,9 @@ echo "Postgres is ready moving on ...">&2
 check_if_couchdb_is_ready(){
   # check if couchdb is up
   wait_count=0
-  until curl -s --head  --request GET $COUCHDB_URL $| grep "200 OK" > /dev/null
+  http_code=$(curl -k --silent --show-error --head "$COUCHDB_URL" --write-out '%{http_code}' | tail -n1)
+  echo "START Checking for cht couchdb at ${COUCHDB_URL}" >&2
+  until [ "$http_code" = "200" ]
   do
     echo "Waiting for cht couchdb" >&2
     wait_count=$((wait_count +1))
